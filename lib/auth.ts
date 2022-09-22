840bd284bc378023eb8b00a8211113b777dfee5c
Amanuel Agegnehu Bekele <32817010+AmanuelAgegnehuBekele@users.noreply.github.com>
2022-09-22 08:05:10 +0300

    Add files via upload

---
+++ lib/auth.ts
@@ -0,0 +1,37 @@
+import jwt from 'jsonwebtoken'
+import { NextApiRequest, NextApiResponse } from 'next'
+import prisma from './prisma'
+
+export const validateRoute = (handler) => {
+    return async (req: NextApiRequest, res: NextApiResponse) => {
+        const { TRAX_ACCESS_TOKEN: token } = req.cookies;
+        if (token) {
+            let user
+            try {
+                const { id } = jwt.verify(token, 'hello')
+                user = await prisma.user.findUnique({
+                    where: { id },
+                })
+                if (!user) {
+                    throw new Error('Not really a user')
+                }
+            } catch (error) {
+                res.status(401).json({
+                    error: 'Not Authorized'
+                })
+                return
+            }
+            return handler(req, res, user);
+        }
+        res.status(401).json({
+            error: 'Not Authorized'
+        })
+
+    }
+}
+
+
+export const validateToken = (token) => {
+    const user = jwt.verify(token, 'hello')
+    return user
+}
